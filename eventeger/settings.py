83a47b54498026bc1cf57fe10bb4c780f0a5f599
Hugo MF <Hpmartinnez@gmail.com>
2022-11-18 16:18:35 +0100

    member tests added

--- eventeger/settings.py
+++ eventeger/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'analytics.middleware.CounterMiddleware',
+    # 'analytics.middleware.CounterMiddleware',
 ]
 
 ROOT_URLCONF = 'eventeger.urls'

a210248430c1216e6a0beaba5221636f546126a4
Hugo MF <Hpmartinnez@gmail.com>
2023-01-11 14:29:03 +0100

    setup one test for admin site

--- eventeger/settings.py
+++ eventeger/settings.py
@@ -49,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'analytics.middleware.CounterMiddleware',
 ]
 
 ROOT_URLCONF = 'eventeger.urls'
